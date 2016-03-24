314ca9af3c1e7ba00d468d25cad3cae64746fac9
theo tran <theotran@rocketmail.com>
2016-03-23 19:59:45 -1000

    initial commit

---
+++ public/js/controllers.js
@@ -0,0 +1,8 @@
+var myApp = angular.module('myApp');
+
+myApp.controller('MyController', [
+  '$scope',
+  function ($scope) {
+    
+  }
+]);
---
+++ public/js/myApp.js
@@ -0,0 +1,15 @@
+angular.module('myApp', ['ngRoute']);
+
+var myApp = angular.module('myApp');
+
+myApp
+  .config(function ($routeProvider ) {
+    
+  })
+  .run([
+    '$rootScope',
+    function ($rootScope) {
+      console.log("start");
+
+    }
+  ]);
