98572177c01ea8f877eff6e198e41fbbe166e830
Chaz Leong <cleong14@gmail.com>
2016-03-23 21:52:21 -1000

    Created working boiler plate express server.

---
+++ server.js
@@ -0,0 +1,11 @@
+var express = require('express');
+
+var app = express();
+
+app.get('/', function (req, res) {
+  res.send('Working');
+});
+
+var server = app.listen(3000, function() {
+  console.log('Listening to port', server.address().port);
+});
