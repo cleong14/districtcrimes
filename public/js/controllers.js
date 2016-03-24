839457eb094f503d377814c4badcf79d50e681ce
theo tran <theotran@rocketmail.com>
2016-03-23 21:57:56 -1000

    added bootstrap

--- public/js/controllers.js
+++ public/js/controllers.js
@@ -1,8 +1,17 @@
-var myApp = angular.module('myApp');
+var myApp = angular.module('myApp', ['ui.bootstrap']);
 
 myApp.controller('MyController', [
   '$scope',
   function ($scope) {
+    //BUTTONS
     
+    // define some random object and button values
+    $scope.bigData = {};
+
+    $scope.bigData.breakfast = false;
+    $scope.bigData.lunch =false;
+
+    // COLLAPSE
+    $scope.isCollapsed = false;
   }
 ]);
