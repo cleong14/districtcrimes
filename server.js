a465ca12cfdc3b399152153ebb065afa52ec97ed
Chaz Leong <cleong14@gmail.com>
2016-03-24 21:06:55 -1000

    temp commit to update branch

--- server.js
+++ server.js
@@ -1,9 +1,16 @@
 var express = require('express');
+var crime = require('crime-dataset');
 
 var app = express();
 
+var db = require('./models');
+
 app.get('/', function (req, res) {
   res.send('Working');
+});
+
+app.get('/crime', function (req, res) {
+  console.log(db);
 });
 
 var server = app.listen(3000, function() {
