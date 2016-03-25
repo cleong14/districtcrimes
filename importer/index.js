9f0c8cb0c458d25535c8077d592811d9b3001d87
Chaz Leong <cleong14@gmail.com>
2016-03-24 21:27:58 -1000

    added import folder and file to import json data into database.

---
+++ importer/index.js
@@ -0,0 +1,13 @@
+var db = require('../models');
+
+
+
+
+
+db.crime.create({
+  date: new Date(1447785240),
+  type: 'MOTOR VEHICLE THEFT',
+  location: 'H3W HALAWA VLY  UP',
+  createdAt: new Date(),
+  updatedAt: new Date()
+});
