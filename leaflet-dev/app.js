dfbaf11af4f42f896adc71c7cb975c1a774a2164
kwhitejr <kwhitejr.edu@gmail.com>
2016-03-24 19:51:10 -1000

    grabbed all district geo data; map is off-center on load

--- leaflet-dev/app.js
+++ leaflet-dev/app.js
@@ -1,57 +1,53 @@
-// var CONFIG = require('config.json');
+// var stateSenateDistrictsGeoJSON = require('Hawaii_State_Senate_Districts.geojson');
 
-var mymap = L.map('mapid').setView([19.78706, 204.52698], 10);
+var mymap = L.map('mapid').setView([19.78706, 204.52698], 1);
+
+var hssdStyle = {
+  "color": "#FFC0CB",
+  "opacity": 0.55,
+  "weight": 2,
+  // "fillColor": "#0000ff",
+  // "fillOpacity": 0.35
+};
+
+var hshdStyle = {
+  "color": "#00FF00",
+  "opacity": 0.55,
+  "weight": 2,
+  // "fillColor": "#0000ff",
+  // "fillOpacity": 0.35
+};
 
 L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
     attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
     maxZoom: 18,
     id: 'mapbox.light',
-    accessToken: 'pk.eyJ1Ijoia3doaXRlanIiLCJhIjoiY2ltNXdqdGFwMDFzanRzbTRwOW52N2syZCJ9.8tgIWcf7d9ZyJ3gjtOssaQ'
+    accessToken: config.CONSTANTS.MAPBOXKEY
 }).addTo(mymap);
 
-// var marker = L.marker([21.264, -157.804])
-//   .addTo(mymap)
-//   .bindPopup("<b>Hello Diamond Head!</b><br>I am pop.")
-//   .openPopup();
+// $('#senate').click(function () {
+//   $.ajax({
+//     dataType: "json",
+//     url: 'http://hawaiistatewidegisopendataportal.histategis.opendata.arcgis.com/datasets/1f335d0805294adf8f3c1260ab86355a_12.geojson',
+//     // data: data,
+//     success: function (data) {
+      L.geoJson(hssd, {
+        style: hssdStyle
+      }).addTo(mymap);
+    // }
+//   });
+// });
 
-// var popup = L.popup();
+$('#house').click(function () {
+  $.ajax({
+    dataType: "json",
+    url: 'http://hawaiistatewidegisopendataportal.histategis.opendata.arcgis.com/datasets/32b91230f14043249c2cdb498a99a446_11.geojson',
+    // data: data,
+    success: function (data) {
+      L.clearLayers().geoJson(data, {
+        style: hshdStyle
+      }).addTo(mymap);
+    }
+  });
+});
 
-// function onMapClick(e) {
-//   popup
-//     .setLatLng(e.latlng)
-//     .setContent("You clicked the map at "+ e.latlng.toString())
-//     .openOn(mymap);
-// }
-
-// mymap.on('click', onMapClick);
-
-// var geojsonFeature = {
-//   "type": "Feature",
-//     "properties": {
-//       "name": "Diamond Head",
-//       "amenity": "Crater",
-//       "popupContent": "A robust hike!"
-//     },
-//     "geometry": {
-//         "type": "Polygon",
-//         "coordinates": [[21.264, -157.804], [22, -158], [21.5, -159]]
-//     }
-// };
-
-var geojsonDiamondHead = {
-  "type": "Feature",
-  "properties": {
-    "style": {
-      weight: 2,
-      color: "#0000ff",
-      opacity: 1,
-      fillColor: "#B0DE5C",
-      fillOpacity: 0.8
-    },
-  },
-  "geometry": {
-    "type": "MultiPolygon",
-  }
-};
-
-L.geoJson(geojsonDiamondHead).addTo(mymap);
