a8553a6c56d9f7300a8774539b132da189f64bd9
Chaz Leong <cleong14@gmail.com>
2016-05-05 18:56:00 -1000

    deleted line chart to test online

--- client/src/js/App.js
+++ client/src/js/App.js
@@ -187,14 +187,7 @@ var App = React.createClass({
           />
         </div>
         <div className="bottomLevel">
-          <LineChart
-            chamber={this.state.chamber}
-            districtData={this.state.districtData}
-            districtNumber={this.state.districtNumber}
-            senateCrimes={this.state.filteredSenateCrimes}
-            houseCrimes={this.state.filteredHouseCrimes}
-            filter={this.state.filter}
-          />
+
           <DonutChart
             filter={this.state.filter}
             districtNumber={this.state.districtNumber}
