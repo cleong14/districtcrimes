b7b8d30b65aabc6bdcc8c2fad7744716d07226e0
Chaz Leong <cleong14@gmail.com>
2016-05-13 01:32:58 -1000

    added host as districtcrimes.com again

--- client/src/js/App.js
+++ client/src/js/App.js
@@ -8,7 +8,7 @@ var Politician = require('./Politician');
 
 var DonutChart = require('./DonutChart');
 
-// var host = 'localhost:3000';
+var host = require('./host');
 
 // where in the actual DOM to mount our App
 var mountNode = document.getElementById('app');
@@ -29,7 +29,7 @@ var App = React.createClass({
 
   loadSenateCrimes: function () {//added
     $.ajax({
-      url: '/senatecrimequery',
+      url: host + '/senatecrimequery',
       method: "GET",
       dataType: "json",
       success: (data) => {
@@ -45,7 +45,7 @@ var App = React.createClass({
   },
   loadHouseCrimes: function () {//added
     $.ajax({
-      url: '/housecrimequery',
+      url: host + '/housecrimequery',
       method: "GET",
       dataType: "json",
       success: (data) => {
@@ -62,7 +62,7 @@ var App = React.createClass({
   loadFile: function (fileName, label) {
     var newState = {};
     $.ajax({
-      url: '/file/'+fileName,
+      url: host + '/file/'+fileName,
       method: "GET",
       dataType: "json",
       success: (data) => {
