57214e8b93b4b3507814c6707326cc2c1a52c677
theo tran <theotran@rocketmail.com>
2016-05-20 12:35:32 -1000

    working on styling for mobile

---
+++ client/src/js/loadingScreen.js
@@ -0,0 +1,14 @@
+    $(window).load(function() {
+        $("#loader").delay(2000).animate({
+            opacity:0,
+            width: 0,
+            height:0
+        }, 500);
+    });
+    
+    //SLIDE UP VERSION
+    // $(window).load(function() {
+    //   $("#loader").delay(3000).animate({
+    //       top: -2000
+    //   }, 1000);
+    // });
